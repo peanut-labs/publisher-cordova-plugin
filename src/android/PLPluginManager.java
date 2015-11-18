adc882d1896ca78da514c7a24c8ac3bd13e0ff05
Bilguun Oyunchimeg <bill.oyunchimeg@gmail.com>
2015-11-18 14:34:43 -0800

    version updated, and some changes on java bridge file

--- src/android/PLPluginManager.java
+++ src/android/PLPluginManager.java
@@ -1,20 +1,34 @@
-package com.peanutlabs.cordova.plugin;
+package com.peanutlabs.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 import com.peanutlabs.plsdk.*;
 
 public class PLPluginManager extends CordovaPlugin {
 	
+	public static final String TAG = "Fucking Apache Cordova";
+	
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+	    super.initialize(cordova, webView);
+		Log.v(TAG,"Plugin initialized");
+	}
 	
 	@override
 	public boolean execute(String action, JSONArray jarray, CallbackContext callbackContext) throws JSONException {
-		PeanutLabsManager pm = PeanutLabsManager.getInstance();
-		pm.setUserId("bilguun_39-8235-64fce826c6");
-		pm.openRewardsCenter(this.cordova.getActivity());
+		Log.v(TAG, action);
+		
+		cordova.getActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				PeanutLabsManager pm = PeanutLabsManager.getInstance();
+				pm.setUserId("bilguun_39-8235-64fce826c6");
+				pm.openRewardsCenter(this.cordova.getActivity());
+			}
+		});
 		
 		callbackContext.success();
 		return true;
