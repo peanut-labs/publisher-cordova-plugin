c97ff75ae2993325648bc335ce7ca85eb570a4ef
Bilguun Oyunchimeg <bill.oyunchimeg@gmail.com>
2015-11-18 13:43:12 -0800

    ....

--- www/peanutplugin.js
+++ www/peanutplugin.js
@@ -1,17 +1,16 @@
-// var argscheck = require('cordova/argscheck');
-// var	exec = require('cordova/exec');
+var exec = require('cordova/exec');
+
+
+// var PeanutlabsPlugin = function() {};
 //
-// module.exports = {
-// 	openRewardsCenter: function(options, successCallback, failureCallback) {
-// 		cordova.exec(successCallback, failureCallback, 'PLPluginManager', 'action', [options]);
-// 	}
+// PeanutlabsPlugin.prototype.openRewardsCenter = function(options, successCallback, failureCallback) {
+// 	return cordova.exec(successCallback, failureCallback, 'PLPluginManager', 'action', [options]);
 // };
 
-
-var PeanutlabsPlugin = function() {};
-
-PeanutlabsPlugin.prototype.openRewardsCenter = function(options, successCallback, failureCallback) {
-	return cordova.exec(successCallback, failureCallback, 'PLPluginManager', 'action', [options]);
+var PeanutlabsPlugin = {
+	openRewardsCenter: function(options, successCallback, failureCallback) {
+		cordova.exec(successCallback, failureCallback, 'PLPluginManager', 'action', [options]);
+	}
 };
 
-module.exports = new PeanutlabsPlugin();
+module.exports = PeanutlabsPlugin;
