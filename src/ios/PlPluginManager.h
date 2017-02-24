aa62b4f01bbc07e3b44ba5404809a3d7449173f2
Alexander Kawrykow <azzro.boy@gmail.com>
2017-02-23 18:08:20 -0800

    Add ios source files to plugin description. Add skeletons for PlPluginManager

---
+++ src/ios/PlPluginManager.h
@@ -0,0 +1,16 @@
+//
+//  PlPluginManager.h
+//
+//  Created by Peanut Labs Inc on 1/10/15.
+//  Copyright (c) 2017 PeanutLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDV.h>
+
+@interface PlPluginManager : CDVPlugin
+
+// Exec API
+- (void)openRewardCenterWithUserId:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/PlPluginManager.m
@@ -0,0 +1,15 @@
+//
+//  PlPluginManager.h
+//
+//  Created by Peanut Labs Inc on 1/10/15.
+//  Copyright (c) 2017 PeanutLabs. All rights reserved.
+//
+
+#import "PlPluginManager.h"
+
+@implementation PlPluginManager
+
+- (void)openRewardCenterWithUserId:(CDVInvokedUrlCommand*)command {
+}
+
+@end
