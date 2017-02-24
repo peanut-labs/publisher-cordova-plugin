f336c3a401050f1f2c8fbb98c39e018c9d84855a
Alexander Kawrykow <azzro.boy@gmail.com>
2017-02-23 19:48:48 -0800

    Filling out remaining logic for openRewardCenterWithUserId

--- src/ios/PlPluginManager.m
+++ src/ios/PlPluginManager.m
@@ -11,12 +11,36 @@
 @implementation PlPluginManager
 
 - (void)openRewardCenterWithUserId:(CDVInvokedUrlCommand*)command {
-  PeanutLabsManager *plManager = [PeanutLabsManager getInstance];
+    PeanutLabsManager *plManager = [PeanutLabsManager getInstance];
+    NSDictionary* data = [command.arguments objectAtIndex:0];
 
-  NSString* userId = [command.arguments objectAtIndex:0];
-  NSString* dob = [command.arguments objectAtIndex:1];
-  [plManager setUserId:userId];
-  [plManager openRewardsCenter];
+    NSString* userId = data[@"user_id"];
+    if ([userId length] == 0){
+        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing user_id argument"] callbackId:command.callbackId];
+        return;
+    } else {
+        [plManager setUserId:userId];
+    }
+
+    NSString* dob = data[@"dob"];
+    if ([dob length] > 0){
+        [plManager setDob:dob];
+    }
+
+    NSString* gender = data[@"gender"];
+    if ([gender length] > 0){
+        [plManager setSex:gender];
+    }
+
+    NSArray* customParameters = data[@"custom_parameters"];
+    if (![customParameters isEqual:[NSNull null]]){
+        for (id param in customParameters){
+            [plManager addCustomParameter:param[@"key"] value:param[@"value"]];
+        }
+    }
+
+    [plManager openRewardsCenter];
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
 }
 
 @end
